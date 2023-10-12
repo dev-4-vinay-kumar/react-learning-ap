e3f5adc31b2d4feec8172b69e4ef5aa10f3bc565
dev-4-vinay-kumar <dev4vinaykumar@gmail.com>
2023-10-12 18:27:08 +0530

    routing commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import Form from './form';
+import Form from './pages/form1';
+import {Routes,Route, BrowserRouter} from "react-router-dom"
+import Form3 from './pages/form3';
+import Form2 from './pages/form2';
 
 function App() {
   return (
-    <Form/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Form/>}></Route>
+      <Route path='Form2' element={<Form2/>}></Route>
+      <Route path='Form3' element={<Form3/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
