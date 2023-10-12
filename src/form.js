75de16fc4c88b168b749751f4e35bcb6ec504649
dev-4-vinay-kumar <dev4vinaykumar@gmail.com>
2023-10-12 09:57:12 +0530

    file upload

---
+++ src/form.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+
+
+
+
+export default function Form() {
+    return (
+        <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg" style={{padding:'20px',marginTop:'50px',backgroundColor:"ButtonHighlight"}}>
+      <Typography variant="h3">Enter Customer Details</Typography>
+      <Grid container columnSpacing={4} rowSpacing={6} style={{ marginTop: '40px' }}>
+          <Grid item xs={12} md={6}>
+          <TextField fullWidth id="outlined-basic" label="Customer Name" variant="outlined"/>
+          </Grid>
+          <Grid item xs={12} md={6} >
+          <TextField fullWidth id="outlined-basic" label="E-Mail" variant="outlined"/>
+          </Grid>
+          <Grid item xs={12} md={6}>
+          <TextField fullWidth id="outlined-basic" label="Bussiness Name" variant="outlined"/>
+          </Grid>
+          <Grid item xs={12} md={6}>
+          <TextField fullWidth id="outlined-basic" label="Phone Number" variant="outlined"/>
+          </Grid>
+          <Grid item xs={12} md={6}>
+          <TextField fullWidth id="outlined-basic" label="City" variant="outlined"/>
+          </Grid>
+          <Grid item xs={12} md={6}>
+          <TextField fullWidth id="outlined-basic" label="Schedule" variant="outlined"/>
+          </Grid>
+          <Grid item xs={12} >
+          <Button variant="contained" color='success' size='large'>SUBMIT</Button>
+          </Grid>
+        </Grid>
+
+      </Container>
+    </React.Fragment>
+    );
+}
