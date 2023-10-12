6ed88eff5731ac7d51fba424228a2d8446c495b7
dev-4-vinay-kumar <dev4vinaykumar@gmail.com>
2023-10-12 18:24:33 +0530

    routing commit

--- src/pages/form1.js
+++ src/pages/form1.js
@@ -3,18 +3,18 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { TextField } from '@mui/material';
+import {TextField } from '@mui/material';
 import Button from '@mui/material/Button';
-
-
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Form() {
+  const navigate=useNavigate()
     return (
         <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg" style={{padding:'20px',marginTop:'50px',backgroundColor:"ButtonHighlight"}}>
-      <Typography variant="h3">Enter Customer Details</Typography>
+      <Typography variant="h3">PAGE 1</Typography>
       <Grid container columnSpacing={4} rowSpacing={6} style={{ marginTop: '40px' }}>
           <Grid item xs={12} md={6}>
           <TextField fullWidth id="outlined-basic" label="Customer Name" variant="outlined"/>
@@ -35,7 +35,7 @@ export default function Form() {
           <TextField fullWidth id="outlined-basic" label="Schedule" variant="outlined"/>
           </Grid>
           <Grid item xs={12} >
-          <Button variant="contained" color='success' size='large'>SUBMIT</Button>
+            <Button onClick={()=>{navigate('/form2')} }  variant="contained" color='success' size='large'>SUBMIT</Button>
           </Grid>
         </Grid>
 
---
+++ src/pages/form3.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import {TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
+export default function Page2(){
+    const navigate=useNavigate()
+    return(
+        <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="lg" style={{padding:'20px',marginTop:'50px',backgroundColor:"ButtonHighlight"}}>
+        <Typography variant="h3">PAGE 3</Typography>
+        <Grid container columnSpacing={4} rowSpacing={6} style={{ marginTop: '40px' }}>
+            <Grid item xs={12} md={6}>
+            <TextField fullWidth id="outlined-basic" label="Customer Name" variant="outlined"/>
+            </Grid>
+            <Grid item xs={12} md={6} >
+            <TextField fullWidth id="outlined-basic" label="E-Mail" variant="outlined"/>
+            </Grid>
+            <Grid item xs={12} md={6}>
+            <TextField fullWidth id="outlined-basic" label="Bussiness Name" variant="outlined"/>
+            </Grid>
+            <Grid item xs={12} md={6}>
+            <TextField fullWidth id="outlined-basic" label="Phone Number" variant="outlined"/>
+            </Grid>
+            <Grid item xs={12} md={6}>
+            <TextField fullWidth id="outlined-basic" label="City" variant="outlined"/>
+            </Grid>
+            <Grid item xs={12} md={6}>
+            <TextField fullWidth id="outlined-basic" label="Schedule" variant="outlined"/>
+            </Grid>
+            <Grid item xs={12} >
+              <Button onClick={()=>{navigate(-1)} }  variant="contained" color='success' size='large'>Back</Button>
+            </Grid>
+          </Grid>
+  
+        </Container>
+      </React.Fragment>
+    )
+}
